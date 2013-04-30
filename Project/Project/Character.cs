fa1af579e74eb54f1e8158944bf7cf7e03391872
Victor Le Cornec <nargrimm@gmail.com>
2013-04-30 18:25:37 +0200

    Animation deplacements fixed

--- Project/Project/Character.cs
+++ Project/Project/Character.cs
@@ -19,7 +19,7 @@ namespace Project
         int vitesse = 2; //test git hub
         public int mapnumber = 5, health, ligne = 1, colonne = 1, mana, healthMax, manaMax, Experience, Strenght, Intelligence, Degat, Armor, Lvl, ExperienceNext;
         public string Direction;
-        int timer = 0,timerRun = 0, i = 0;
+        int timer = 0, timerRun = 0, i = 0;
         public bool fight = false, lvlup;
         public Map map, map4, map5;
 
@@ -61,255 +61,243 @@ namespace Project
             }
             if (!fight)
             {
-                if (KState.IsKeyDown(Keys.Q))
+                //Left
+                if (KState.IsKeyDown(Keys.Q) && KState.IsKeyUp(Keys.LeftShift))
                 {
-                    if (KState.IsKeyDown(Keys.LeftShift))
+                    ligne = 1;
+                    timer++;
+                    if (timer == 15)
                     {
-                        ligne = 6;
-                        timerRun++;
-                        if (timerRun == 20)
+                        timer = 0;
+                        if (colonne == 5)
                         {
-                            timerRun = 0;
-                            if (colonne == 5)
-                            {
-                                colonne = 1;
-                            }
-                            if (colonne == 3)
-                            {
-                                colonne = 1;
-                            }
-                            else
-                            {
-                                colonne++;
-                            }
-
-                        }
-                        Rectsprite = new Rectangle((colonne) * 32, (ligne) * 63, 30, 63);
-                        if (persoPosition.X <= 0)
-                        {
-                            map = map4;
-                            persoPosition.X = (screenWidth - persoTexture.Width / 6);
+                            colonne = 2;
                         }
                         else
                         {
-                            persoPosition += (new Vector2((-2 * vitesse), 0));
+                            colonne++;
                         }
+                    }
+                    Rectsprite = new Rectangle((colonne) * 32, (ligne) * 63, 30, 63);
+                    if (persoPosition.X <= 0)
+                    {
+                        map = map4;
+                        persoPosition.X = (screenWidth - persoTexture.Width / 6);
                     }
                     else
                     {
-                        ligne = 1;
-                        timer++;
-                        if (timer == 15)
-                        {
-                            timer = 0;
-                            if (colonne == 5)
-                            {
-                                colonne = 2;
-                            }
-                            else
-                            {
-                                colonne++;
-                            }
-
-                        }
-                        Rectsprite = new Rectangle((colonne) * 32, (ligne) * 63, 30, 63);
-                        if (persoPosition.X <= 0)
-                        {
-                            map = map4;
-                            persoPosition.X = (screenWidth - persoTexture.Width / 6);
-                        }
-                        else
-                        {
-                            persoPosition += (new Vector2((-vitesse), 0));
-                        }
+                        persoPosition += (new Vector2((-vitesse), 0));
                     }
 
                 }
-                else if (KState.IsKeyDown(Keys.Z))
+                
+                else if (KState.IsKeyDown(Keys.LeftShift) && KState.IsKeyDown(Keys.Q))
                 {
-                    if (KState.IsKeyDown(Keys.LeftShift))
+                    ligne = 6;
+                    timerRun++;
+                    if (timerRun == 20)
                     {
-                        ligne = 5;
-                        timerRun++;
-                        if (timerRun == 20)
+                        timerRun = 0;
+                        if (colonne == 5)
                         {
-                            timerRun = 0;
-                            if (colonne == 5)
-                            {
-                                colonne = 1;
-                            }
-                            else
-                            {
-                                colonne++;
-                            }
-
+                            colonne = 1;
                         }
-                        Rectsprite = new Rectangle(colonne * 32, ligne * 63, 30, 63);
-                        if (persoPosition.Y <= 0)
+                        if (colonne == 3)
                         {
-                            mapnumber += 3;
-                            persoPosition.Y = screenHeight - persoTexture.Height / 8;
+                            colonne = 1;
                         }
                         else
                         {
-                            persoPosition += new Vector2(0, -2 * vitesse);
+                            colonne++;
                         }
+                    }
+                    Rectsprite = new Rectangle((colonne) * 32, (ligne) * 63, 30, 63);
+                    if (persoPosition.X <= 0)
+                    {
+                        map = map4;
+                        persoPosition.X = (screenWidth - persoTexture.Width / 6);
                     }
                     else
                     {
-                        ligne = 2;
-                        timer++;
-                        if (timer == 15)
-                        {
-                            timer = 0;
-                            if (colonne == 3)
-                            {
-                                colonne = 1;
-                            }
-                            else
-                            {
-                                colonne++;
-                            }
-
-                        }
-                        Rectsprite = new Rectangle(colonne * 32, ligne * 63, 30, 63);
-                        if (persoPosition.Y <= 0)
-                        {
-                            mapnumber += 3;
-                            persoPosition.Y = screenHeight - persoTexture.Height / 8;
-                        }
-                        else
-                        {
-                            persoPosition += new Vector2(0, -vitesse);
-                        }
-                    }
-
-                }
-                else if (KState.IsKeyDown(Keys.S))
-                {
-                    if (KState.IsKeyDown(Keys.LeftShift))
-                    {
-                        timerRun++;
-                        ligne = 4;
-                        if (timerRun == 20)
-                        {
-                            timerRun = 0;
-                            if (colonne == 5)
-                            {
-                                colonne = 1;
-                            }
-                            else
-                            {
-                                colonne++;
-                            }
-
-                        }
-                        Rectsprite = new Rectangle(colonne * 32, ligne * 63, 30, 62);
-                        if (persoPosition.Y >= (screenHeight - persoTexture.Height / 8))
-                        {
-                            mapnumber -= 3;
-                            persoPosition.Y = 0;
-                        }
-                        else
-                        {
-                            persoPosition += new Vector2(0, 2 * vitesse);
-                        }
-
-                    }
-                    else
-                    {
-                        timer++;
-                        ligne = 0;
-                        if (timerRun == 15)
-                        {
-                            timer = 0;
-                            if (colonne == 5)
-                            {
-                                colonne = 1;
-                            }
-                            else
-                            {
-                                colonne++;
-                            }
-
-                        }
-                        Rectsprite = new Rectangle(colonne * 32, ligne * 63, 30, 62);
-                        if (persoPosition.Y >= (screenHeight - persoTexture.Height / 8))
-                        {
-                            mapnumber -= 3;
-                            persoPosition.Y = 0;
-                        }
-                        else
-                        {
-                            persoPosition += new Vector2(0, vitesse);
-                        }
+                        persoPosition += (new Vector2((-2 * vitesse), 0));
                     }
                 }
-                else if (KState.IsKeyDown(Keys.D))
+
+                //Up
+                else if (KState.IsKeyDown(Keys.Z) && KState.IsKeyUp(Keys.LeftShift))
                 {
-                    if (KState.IsKeyDown(Keys.LeftShift))
+                    ligne = 2;
+                    timer++;
+                    if (timer == 15)
                     {
-                        timerRun++;
-                        ligne = 7;
-
-                        if (timerRun == 20)
+                        timer = 0;
+                        if (colonne == 3)
                         {
-                            timerRun = 0;
-
-                            if (colonne == 5)
-                            {
-                                colonne = 1;
-                            }
-                            if (colonne == 3)
-                            {
-                                colonne = 1;
-                            }
-                            else
-                            {
-                                colonne++;
-                            }
-                        }
-                        Rectsprite = new Rectangle(colonne * 32, ligne * 63, 30, 63);
-                        if (persoPosition.X >= (screenWidth - persoTexture.Width / 6))
-                        {
-                            mapnumber += 1;
-                            persoPosition.X = 0;
+                            colonne = 1;
                         }
                         else
                         {
-                            persoPosition += new Vector2(2 * vitesse, 0);
+                            colonne++;
                         }
-
+                    }
+                    Rectsprite = new Rectangle(colonne * 32, ligne * 63, 30, 63);
+                    if (persoPosition.Y <= 0)
+                    {
+                        mapnumber += 3;
+                        persoPosition.Y = screenHeight - persoTexture.Height / 8;
                     }
                     else
                     {
-                        timer++;
-                        ligne = 3;
-                        if (timer == 15)
+                        persoPosition += new Vector2(0, -vitesse);
+                    }
+                }
+                
+                else if (KState.IsKeyDown(Keys.LeftShift) && KState.IsKeyDown(Keys.Z))
+                {
+                    ligne = 5;
+                    timerRun++;
+                    if (timerRun == 20)
+                    {
+                        timerRun = 0;
+                        if (colonne == 5)
                         {
-                            timer = 0;
-                            if (colonne == 5)
-                            {
-                                colonne = 2;
-                            }
-                            else
-                            {
-                                colonne++;
-                            }
-                        }
-                        Rectsprite = new Rectangle(colonne * 32, ligne * 63, 30, 63);
-                        if (persoPosition.X >= (screenWidth - persoTexture.Width / 6))
-                        {
-                            mapnumber += 1;
-                            persoPosition.X = 0;
+                            colonne = 1;
                         }
                         else
                         {
-                            persoPosition += new Vector2(vitesse, 0);
+                            colonne++;
+                        }
+                    }
+                    Rectsprite = new Rectangle(colonne * 32, ligne * 63, 30, 63);
+                    if (persoPosition.Y <= 0)
+                    {
+                        mapnumber += 3;
+                        persoPosition.Y = screenHeight - persoTexture.Height / 8;
+                    }
+                    else
+                    {
+                        persoPosition += new Vector2(0, -2 * vitesse);
+                    }
+                }
+
+                //Down
+                else if (KState.IsKeyDown(Keys.S) && KState.IsKeyUp(Keys.LeftShift))
+                {
+                    timer++;
+                    ligne = 0;
+                    if (timer == 15)
+                    {
+                        timer = 0;
+                        if (colonne == 5)
+                        {
+                            colonne = 1;
+                        }
+                        else
+                        {
+                            colonne++;
+                        }
+                    }
+                    Rectsprite = new Rectangle(colonne * 32, ligne * 63, 30, 62);
+                    if (persoPosition.Y >= (screenHeight - persoTexture.Height / 8))
+                    {
+                        mapnumber -= 3;
+                        persoPosition.Y = 0;
+                    }
+                    else
+                    {
+                        persoPosition += new Vector2(0, vitesse);
+                    }
+                }
+
+                else if (KState.IsKeyDown(Keys.LeftShift) && KState.IsKeyDown(Keys.S))
+                {
+                    timerRun++;
+                    ligne = 4;
+                    if (timerRun == 20)
+                    {
+                        timerRun = 0;
+                        if (colonne == 5)
+                        {
+                            colonne = 1;
+                        }
+                        else
+                        {
+                            colonne++;
                         }
 
                     }
+                    Rectsprite = new Rectangle(colonne * 32, ligne * 63, 30, 62);
+                    if (persoPosition.Y >= (screenHeight - persoTexture.Height / 8))
+                    {
+                        mapnumber -= 3;
+                        persoPosition.Y = 0;
+                    }
+                    else
+                    {
+                        persoPosition += new Vector2(0, 2 * vitesse);
+                    }
+                }
+                //right
+                else if (KState.IsKeyDown(Keys.D) && KState.IsKeyUp(Keys.LeftShift))
+                {
+                    timer++;
+                    ligne = 3;
+                    if (timer == 15)
+                    {
+                        timer = 0;
+                        if (colonne == 5)
+                        {
+                            colonne = 2;
+                        }
+                        else
+                        {
+                            colonne++;
+                        }
+                    }
+                    Rectsprite = new Rectangle(colonne * 32, ligne * 63, 30, 63);
+                    if (persoPosition.X >= (screenWidth - persoTexture.Width / 6))
+                    {
+                        mapnumber += 1;
+                        persoPosition.X = 0;
+                    }
+                    else
+                    {
+                        persoPosition += new Vector2(vitesse, 0);
+                    }
+                }
 
+                else if (KState.IsKeyDown(Keys.LeftShift) && KState.IsKeyDown(Keys.D))
+                {
+                    timerRun++;
+                    ligne = 7;
+
+                    if (timerRun == 20)
+                    {
+                        timerRun = 0;
+
+                        if (colonne == 5)
+                        {
+                            colonne = 1;
+                        }
+                        if (colonne == 3)
+                        {
+                            colonne = 1;
+                        }
+                        else
+                        {
+                            colonne++;
+                        }
+                    }
+                    Rectsprite = new Rectangle(colonne * 32, ligne * 63, 30, 63);
+                    if (persoPosition.X >= (screenWidth - persoTexture.Width / 6))
+                    {
+                        mapnumber += 1;
+                        persoPosition.X = 0;
+                    }
+                    else
+                    {
+                        persoPosition += new Vector2(2 * vitesse, 0);
+                    }
                 }
 
             }
